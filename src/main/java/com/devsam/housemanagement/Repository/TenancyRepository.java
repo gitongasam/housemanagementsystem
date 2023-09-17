2e39da05cc1ad5fbdf38760caaf5fc6d50d68803
Developer sam <gitongas684@gmail.com>
2023-09-17 10:07:10 +0300

    Revert "Revert "made property manager have property using one to many""
    This reverts commit 8704584a76bfee48c552ed09b4e7d1098038f4e7.

---
+++ src/main/java/com/devsam/housemanagement/Repository/TenancyRepository.java
@@ -0,0 +1,6 @@
+package com.devsam.housemanagement.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TenancyRepository extends JpaRepository<Tenancy, Long> {
+}
